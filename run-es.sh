7e9c4b2e125e3e601280de7be496fb5a7f030cbb
dotcomstu <stuart@rednut.net>
2014-12-03 23:03:51 +0000

    upgrade to 1.4

--- run-es.sh
+++ run-es.sh
@@ -1,1 +1,24 @@
-docker run -d --name="elasticsearch" -p 9200:9200 -p 9300:9300 -v /docker/elasticsearch:/data rednut/elasticsearch /elasticsearch/bin/elasticsearch -Des.config=/data/elasticsearch.yml
+#!/usr/bin/env bash
+set -e
+set -x
+
+NODE_NAME=es-s2-localdisk
+CONTAINER_NAME=elasticsearch-1
+VOLUME=/docker/elasticsearch-1
+CLUSTER_NAME=rednut-dev
+
+
+docker rm -f $CONTAINER_NAME || echo "not a container yet: '$CONTAINER_NAME'"
+
+
+docker run \
+        -d \
+        --name="$CONTAINER_NAME" \
+        -p 127.0.0.1:9200:9200 \
+        -p 127.0.0.1:9300:9300 \
+        -v $VOLUME:/data \
+        rednut/elasticsearch \
+        /elasticsearch/bin/elasticsearch \
+		-Des.config=/data/elasticsearch.yml \
+                -Des.cluster.name=$CLUSTER_NAME \
+		-Des.node.name=$NODE_NAME
